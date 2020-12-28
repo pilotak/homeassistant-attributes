b1d554b7124693d431fcb9fb26f36f290a5531a7
Pavel Slama <info@pavelslama.cz>
2020-12-28 11:09:20 +0100

    version bump

--- custom_components/attributes/sensor.py
+++ custom_components/attributes/sensor.py
@@ -19,7 +19,7 @@ from homeassistant.helpers.restore_state import RestoreEntity
 from homeassistant.helpers import template as template_helper
 from homeassistant.util import slugify
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 
 _LOGGER = logging.getLogger(__name__)
 
